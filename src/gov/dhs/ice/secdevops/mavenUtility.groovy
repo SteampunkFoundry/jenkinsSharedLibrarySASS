5823aab1616fec6689ab8ade3a23bc51002e228a
Jim McCarthy <72879804+mccartjh@users.noreply.github.com>
2022-03-03 07:42:08 -0500

    Update mavenUtility.groovy
    Changed the name of the service to match the Jenkins configuration.

--- src/gov/dhs/ice/secdevops/mavenUtility.groovy
+++ src/gov/dhs/ice/secdevops/mavenUtility.groovy
@@ -25,7 +25,7 @@ class mavenUtility implements Serializable {
                 containers: [
                         steps.containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl', ttyEnabled: true, command: '/bin/cat')
                 ],
-                serviceAccount: 'jenkins-sa'
+                serviceAccount: 'jenkins'
         ) {
             steps.node(kubelabel) {
                 steps.stage('cache check') {
